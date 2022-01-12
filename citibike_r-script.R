bf906bbe651ea879c48f8716d28bef5e09b72eef
Ashutosh Rajput <86178406+AshutoshR25@users.noreply.github.com>
2022-01-12 16:49:02 +0530

    Add files via upload

---
+++ citibike_r-script.R
@@ -0,0 +1,16 @@
+
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("lubridate")
+
+
+library(tidyverse)  #helps wrangle data
+library(lubridate)  #helps wrangle date attributes
+library(ggplot2)  #helps visualize data
+
+
+setwd("C:/Users/ashut/Desktop/My_projects/Google_data_analytics/Case_study-Citing Cyclistic Bike to Increase annual members/Divvy_trips_datasets-2019-2020")
+
+getwd()
+
+
